acaaf58b6c19815dd15f05528a7417a53a923766
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-07-03 20:44:53 +0930

    Update Util.js

--- src/Structures/Util.js
+++ src/Structures/Util.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { promisify } = require('util');
 const glob = promisify(require('glob'));
 const Command = require('./Command.js');
+const Event = require('./Event.js');
 
 module.exports = class Util {
 
@@ -27,12 +28,20 @@ module.exports = class Util {
 		}
 		return arr;
 	}
-	
+
 	formatBytes(bytes) {
 		if (bytes === 0) return '0 Bytes';
 		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 		const i = Math.floor(Math.log(bytes) / Math.log(1024));
 		return `${parseFloat((bytes / Math.pow(1024, i)).toFixed(2))} ${sizes[i]}`;
+	}
+
+	removeDuplicates(arr) {
+		return [...new Set(arr)];
+	}
+
+	capitalise(string) {
+		return string.split(' ').map(str => str.slice(0, 1).toUpperCase() + str.slice(1)).join(' ');
 	}
 
 	async loadCommands() {
