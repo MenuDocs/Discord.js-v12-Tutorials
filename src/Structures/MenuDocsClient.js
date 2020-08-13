869a1a5cb05851fd15d801e3dc1479d298f17c95
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-08-13 16:37:29 +0930

    Update MenuDocsClient.js

--- src/Structures/MenuDocsClient.js
+++ src/Structures/MenuDocsClient.js
@@ -13,33 +13,11 @@ module.exports = class MenuDocsClient extends Client {
 
 		this.aliases = new Collection();
 
+		this.events = new Collection();
+
 		this.utils = new Util(this);
 
-		this.once('ready', () => {
-			console.log(`Logged in as ${this.user.username}!`);
-		});
-
-		this.on('message', async (message) => {
-			const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
-			const mentionRegexPrefix = RegExp(`^<@!${this.user.id}> `);
-
-			if (!message.guild || message.author.bot) return;
-
-			if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${this.prefix}\`.`);
-
-			const prefix = message.content.match(mentionRegexPrefix) ?
-				message.content.match(mentionRegexPrefix)[0] : this.prefix;
-			
-			if(!message.content.startsWith(prefix)) return;
-
-			// eslint-disable-next-line no-unused-vars
-			const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
-
-			const command = this.commands.get(cmd.toLowerCase()) || this.commands.get(this.aliases.get(cmd.toLowerCase()));
-			if (command) {
-				command.run(message, args);
-			}
-		});
+		this.owners = options.owners;
 	}
 
 	validate(options) {
@@ -55,6 +33,7 @@ module.exports = class MenuDocsClient extends Client {
 
 	async start(token = this.token) {
 		this.utils.loadCommands();
+		this.utils.loadEvents();
 		super.login(token);
 	}
 
