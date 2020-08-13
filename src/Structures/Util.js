e4fa2adfd944908ddb98b705900881c8a6600f5f
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-08-13 16:37:50 +0930

    Update Util.js

--- src/Structures/Util.js
+++ src/Structures/Util.js
@@ -63,4 +63,19 @@ module.exports = class Util {
 		});
 	}
 
+	async loadEvents() {
+		return glob(`${this.directory}events/**/*.js`).then(events => {
+			for (const eventFile of events) {
+				delete require.cache[eventFile];
+				const { name } = path.parse(eventFile);
+				const File = require(eventFile);
+				if (!this.isClass(File)) throw new TypeError(`Event ${name} doesn't export a class!`);
+				const event = new File(this.client, name);
+				if (!(event instanceof Event)) throw new TypeError(`Event ${name} doesn't belong in Events`);
+				this.client.events.set(event.name, event);
+				event.emitter[event.type](name, (...args) => event.run(...args));
+			}
+		});
+	}
+
 };
