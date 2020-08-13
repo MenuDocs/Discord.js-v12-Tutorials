e0dc0ec923d596ff73302788ff77fdd0f5c1893f
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-08-13 16:36:29 +0930

    Add files via upload

---
+++ src/Events/message/message.js
@@ -0,0 +1,24 @@
+const Event = require('../../Structures/Event');
+
+module.exports = class extends Event {
+
+	async run(message) {
+		const mentionRegex = RegExp(`^<@!${this.client.user.id}>$`);
+		const mentionRegexPrefix = RegExp(`^<@!${this.client.user.id}> `);
+
+		if (!message.guild || message.author.bot) return;
+
+		if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${this.client.prefix}\`.`);
+
+		const prefix = message.content.match(mentionRegexPrefix) ?
+			message.content.match(mentionRegexPrefix)[0] : this.client.prefix;
+
+		const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+
+		const command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
+		if (command) {
+			command.run(message, args);
+		}
+	}
+
+};
---
+++ src/Events/ready.js
@@ -0,0 +1,19 @@
+const Event = require('../Structures/Event');
+
+module.exports = class extends Event {
+
+	constructor(...args) {
+		super(...args, {
+			once: true
+		});
+	}
+
+	run() {
+		console.log([
+			`Logged in as ${this.client.user.tag}`,
+			`Loaded ${this.client.commands.size} commands!`,
+			`Loaded ${this.client.events.size} events!`
+		].join('\n'));
+	}
+
+};
