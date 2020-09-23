913573ae58420d26a9f1890b7b83a5ec1e32b048
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-09-23 12:04:21 +0930

    Episode 9 going live

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -14,6 +14,15 @@ module.exports = class extends Event {
 			`Loaded ${this.client.commands.size} commands!`,
 			`Loaded ${this.client.events.size} events!`
 		].join('\n'));
+
+		const activities = [
+			`${this.client.guilds.cache.size} servers!`,
+			`${this.client.channels.cache.size} channels!`,
+			`${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users!`
+		];
+
+		let i = 0;
+		setInterval(() => this.client.user.setActivity(`${this.client.prefix}help | ${activities[i++ % activities.length]}`, { type: 'WATCHING' }), 15000);
 	}
 
 };
