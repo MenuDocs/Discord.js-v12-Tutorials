18aa775916c9b147c2ad2d73c6898487b7765200
Connor Moriarty <42255832+Strandxo@users.noreply.github.com>
2020-10-09 15:22:03 +1030

    Update message.js

--- src/Events/message/message.js
+++ src/Events/message/message.js
@@ -3,21 +3,42 @@ const Event = require('../../Structures/Event');
 module.exports = class extends Event {
 
 	async run(message) {
-		const mentionRegex = RegExp(`^<@!${this.client.user.id}>$`);
-		const mentionRegexPrefix = RegExp(`^<@!${this.client.user.id}> `);
+		const mentionRegex = RegExp(`^<@!?${this.client.user.id}>$`);
+		const mentionRegexPrefix = RegExp(`^<@!?${this.client.user.id}> `);
 
-		if (!message.guild || message.author.bot) return;
+		if (message.author.bot) return;
 
 		if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is \`${this.client.prefix}\`.`);
 
 		const prefix = message.content.match(mentionRegexPrefix) ?
 			message.content.match(mentionRegexPrefix)[0] : this.client.prefix;
+		
+		if (!message.content.startsWith(prefix)) return;
 
 		const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
 
 		const command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
 		if (command) {
-			command.run(message, args);
+			
+			if(message.guild) {
+				const userPermCheck = command.userPerms ? this.client.defaultPerms.add(command.userPerms) : this.client.defaultPerms;
+				if (userPermCheck) {
+					const missing = message.channel.permissionsFor(message.member).missing(userPermCheck);
+					if (missing.length) {
+						return message.reply(`You are missing ${this.client.utils.formatArray(missing.map(this.client.utils.formatPerms))} permissions, you need them to use this command!`);
+					}
+				}
+
+				const botPermCheck = command.botPerms ? this.client.defaultPerms.add(command.botPerms) : this.client.defaultPerms;
+				if (botPermCheck) {
+					const missing = message.channel.permissionsFor(this.client.user).missing(botPermCheck);
+					if (missing.length) {
+						return message.reply(`I am missing ${this.client.utils.formatArray(missing.map(this.client.utils.formatPerms))} permissions, I need them to run this command!`);
+					}
+				}
+			}
+			
+			command.run(message, ...args);
 		}
 	}
 
