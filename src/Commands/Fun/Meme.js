3eb44ec57d30f7f33e2b10fb25438a58a9849b72
Leo <47292121+NullPointDev@users.noreply.github.com>
2021-01-13 12:24:01 +1000

    Fix Image Commands
    Filter results on all image commands to ensure that embeds don't break.

--- src/Commands/Fun/Meme.js
+++ src/Commands/Fun/Meme.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
 	async run(message) {
 		const data = await fetch(`https://imgur.com/r/${subreddits[Math.floor(Math.random() * subreddits.length)]}/hot.json`)
 			.then(response => response.json())
-			.then(body => body.data);
+			.then(body => body.data.filter(post => post.mimetype.startsWith("image")));
 		const selected = data[Math.floor(Math.random() * data.length)];
 		return message.channel.send(new MessageEmbed().setImage(`https://imgur.com/${selected.hash}${selected.ext.replace(/\?.*/, '')}`));
 	}
